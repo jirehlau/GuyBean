ec8c3cb2414e3b13615fec0348772d7133d4e413
Jireh Lau <jirehcylau@gmail.com>
2021-03-14 01:05:27 -0500

    routes added

--- GuyBean/routes/users.js
+++ GuyBean/routes/users.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+var router = require('express').Router();
+var userCtrl = require('../controllers/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/students', userCtrl.index);
+
+router.post('/facts', isLoggedIn, userCtrl.addFact);
+
+router.delete('/facts/:id', userCtrl.delFact);
+
+function isLoggedIn(req, res, next) {
+  if ( req.isAuthenticated() ) return next();
+  res.redirect('/auth/google');
+}
 
 module.exports = router;
