e244e254249cd0d1f709484e3c5781e3de977e92
Jireh Lau <jirehcylau@gmail.com>
2021-03-15 10:56:33 -0400

    update

--- GuyBean/server.js
+++ GuyBean/server.js
@@ -12,6 +12,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// CONGIURE THE 'DOTENV' PACKAGE TO READ .ENV FILE
+let dotenv = require('dotenv')
+dotenv.config()
+
 var app = express();
 
 require('./config/database');
