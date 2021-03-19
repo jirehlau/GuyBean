b49c6812f878e5f4b573426b49fe78be1252d866
Jireh Lau <jirehcylau@gmail.com>
2021-03-19 01:09:43 -0400

    Heroku 3rd push

--- routes/users.js
+++ routes/users.js
@@ -25,7 +25,7 @@ router.get('/myrestaurants/:id',userCtrl.myRestaurantsDetails)
 
 router.get('/sushiOne',userCtrl.sushiOne)
 
-router.post('/addedrestaurant', userCtrl.addedRestaurant)
+router.post('/myrestaurants', userCtrl.addedRestaurant)
 router.post('/facts', isLoggedIn, userCtrl.addFact);
 
 router.delete('/facts/:id', userCtrl.delFact);
